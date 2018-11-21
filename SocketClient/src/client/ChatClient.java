9137b5f07486e09005f226bf8ebf29dcb1de1ff2
DaeIn Lim <dnfwlxo11@naver.com>
2018-11-21 17:52:53 +0900

    "Daein Commit, added Timer and close button event source"

--- SocketClient/src/client/ChatClient.java
+++ SocketClient/src/client/ChatClient.java
@@ -17,11 +17,14 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class ChatClient extends Application{
 	static Socket socket;
 	static HashMap<Integer, String> quizDB = new HashMap<Integer, String>();
-	static String result = "0";
+	static String result = "";
+	static int count = 0; static int correctResult = 0; static int incorrectResult = 0;
+	static String userResult = "";
 	static String chatLogtemp = "";
 	
 	VBox root = new VBox();
@@ -127,6 +130,10 @@ public class ChatClient extends Application{
 	    	    	}catch(Exception e){
 	    	    		e.printStackTrace();
 	    	    	}
+	    	    	try {Thread.sleep(50);} catch(Exception e2) {} 
+					Quiz.interrupted();
+					try {Thread.sleep(50);} catch(Exception e2) {} 
+					Timer.interrupted();
 	    	    	stopClient();
 	    	    	stage.hide();
 	    	    }
@@ -143,6 +150,19 @@ public class ChatClient extends Application{
 		    		}
 	    	});
 	    	
+	    	stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+	            public void handle(WindowEvent we) {
+	            	send("quit" + userName);
+	    	    	try{
+	    	    		Thread.sleep(100);
+	    	    	}catch(Exception e){
+	    	    		e.printStackTrace();
+	    	    	}
+	    	    	stopClient();
+	    	    	stage.hide();
+	            }
+	    	});
+	    	
 	    	ObservableList<Node> list = root.getChildren();
 	    	list.add(rootMessage);
 	    	
@@ -157,6 +177,81 @@ public class ChatClient extends Application{
     	} catch(Exception e) {
     		System.out.println(e);
     	}
+	}
+	
+	static class Quiz extends Thread{
+		public static void startQuiz(String temp) {
+			Thread thread = new Thread() {
+				public void run() {
+					Timer timer = new Timer();
+					try {
+						timer.startTimer();
+						
+						chatlog.setText("");
+						chatLogtemp = chatlog.getText();	
+						chatlog.appendText(quizDB.get(1) + "\n");
+						
+						sleep(6000);
+						
+						count++;
+						if(count == 6) {
+							count = 0;
+							timerField.setText("");
+							System.out.println("정답 : " + correctResult + ", 오답 : " + incorrectResult);
+							correctResult = 0;
+							incorrectResult = 0;
+							chatlog.setText(chatLogtemp);
+							try {sleep(50);} catch(Exception e) {} Timer.interrupted();
+							try {sleep(50);} catch(Exception e) {} Quiz.interrupted();
+							return;
+						}
+						
+						try {sleep(100);} catch(Exception e) {}
+						send("전공");
+					} catch (Exception e) {
+						try {sleep(50);} catch(Exception e2) {} 
+						Quiz.interrupted();
+					}
+				}
+			};
+			
+			thread.start();
+		}
+	}
+	
+	static class Timer extends Thread{
+		public void startTimer() {
+			Thread thread = new Thread() {
+				public void run() {
+					int second = 5;
+					while(second >= 0) {						
+						try {
+							timerField.setText(" " + Integer.toString(second) + "초");
+							sleep(1000);
+							second--;
+						} catch (Exception e) {
+							Timer.interrupted();
+						}
+					}
+				}
+			};
+			
+			thread.start();
+		}
+	}
+	
+	public static boolean mark(String quizResult) {
+		if(result.equals(quizResult)) {
+			correctResult++;
+			userResult = "";
+			return true;
+		}
+		
+		else {
+			incorrectResult++;
+			userResult = "";
+			return false;
+		}
 	}
 	
 	public static void runQuiz() {
@@ -192,19 +287,17 @@ public class ChatClient extends Application{
 							quizDB.put(1, message.split("\\^")[2]);
 							result = message.split("\\^")[3];
 							System.out.println(result);
-						}
-						
-						else if(message.length() >= 10 && message.substring(0, 10).equals("mstartquiz")) {
-							runQuiz();	
+							Quiz.startQuiz(result);
 						}
 						
 						else if(message.length() >= 3 && message.substring(0, 3).equals("m답:")) {
 							System.out.println(message.substring(3));
 							if(mark(message.substring(3))) {
-								send("정답입니다.");
+								send("정답입니다.\n");
 							}
+							
 							else {
-								send("오답입니다.");
+								send("오답입니다.\n");
 							}
 							
 						}
@@ -239,13 +332,6 @@ public class ChatClient extends Application{
 				break;
 			}
 		}
-	}
-	
-	public static boolean mark(String quizResult) {
-		if(quizResult.equals(result))
-			return true;
-		else
-			return false;
 	}
 	
 	public static void send(String message) {
